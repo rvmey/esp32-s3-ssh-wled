379b653c6269acdea5bfcaafaa15a1f3a03e9a32
Russell VanderMey <russell@vandermey.us>
2026-05-25 10:02:37 -0400

    Core2 firmware 2.0.207: expand TriggerCMD CA chain

--- main/main.c
+++ main/main.c
@@ -28,7 +28,7 @@
 #include "screen_control.h"
 #endif
 
-#define APP_VERSION "2.0.206"
+#define APP_VERSION "2.0.207"
 const char g_firmware_version[] = APP_VERSION;
 
 static const char *TAG = "main";
