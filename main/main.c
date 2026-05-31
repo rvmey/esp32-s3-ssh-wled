c79381d212e918ce7b6a31aa03e05d38b9773095
Russell VanderMey <russell@vandermey.us>
2026-05-31 19:09:28 -0400

    Core2: switch LED GPIO 25 -> 26 (Grove Port B data pin) (v2.0.298)

--- main/main.c
+++ main/main.c
@@ -30,7 +30,7 @@
 #include "screen_control.h"
 #endif
 
-#define APP_VERSION "2.0.297"
+#define APP_VERSION "2.0.298"
 const char g_firmware_version[] = APP_VERSION;
 
 static const char *TAG = "main";
