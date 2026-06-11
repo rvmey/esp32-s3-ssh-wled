87179c4ad99dd8c75332c89b7c249b250aaea405
Russell VanderMey <russell@vandermey.us>
2026-06-11 08:21:23 -0400

    Add CYD-specific display driver path and bump to 2.0.392

--- main/main.c
+++ main/main.c
@@ -30,7 +30,7 @@
 #include "screen_control.h"
 #endif
 
-#define APP_VERSION "2.0.391"
+#define APP_VERSION "2.0.392"
 const char g_firmware_version[] = APP_VERSION;
 
 static const char *TAG = "main";
